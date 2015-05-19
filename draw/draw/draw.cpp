8affbc3319008240bc5786c2da8ee0cf3c11bb22
Krzysztof Dudziak <krz.dudziak@gmail.com>
2015-05-19 09:34:12 +0200

    Test2

--- draw/draw/draw.cpp
+++ draw/draw/draw.cpp
@@ -281,7 +281,6 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 	UNREFERENCED_PARAMETER(lParam);
 	switch (message)
 	{
-
 	case WM_INITDIALOG:
 		return (INT_PTR)TRUE;
 
